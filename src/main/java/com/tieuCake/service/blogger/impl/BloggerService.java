4c045c3c553ac278b658ec901f279d1b42e76a3e
aluhaNguyen <nguyenmaihung127@gmail.com>
2018-08-26 23:54:58 +0700

    delete

--- src/main/java/com/tieuCake/service/blogger/impl/BloggerService.java
+++ src/main/java/com/tieuCake/service/blogger/impl/BloggerService.java
@@ -42,139 +42,142 @@ import java.util.stream.Collectors;
  */
 @Service
 public class BloggerService implements IBloggerService {
-    // Configure the Java API Client for Installed Native App
-    private Logger logger = Logger.getLogger("BloggerService");
+	// Configure the Java API Client for Installed Native App
+	private Logger logger = Logger.getLogger("BloggerService");
 
-    @Autowired
-    private IPostDAO postDAO;
+	@Autowired
+	private IPostDAO postDAO;
 
-    @Autowired
-    private IAuthorizationService authorizationService;
+	@Autowired
+	private IAuthorizationService authorizationService;
 
-    private Blogger blogger = null;
+	private Blogger blogger = null;
 
+	public Blogger getBlogger() throws IOException, InvalidValueException {
+		if (null == blogger) {
+			GoogleCredential credential = authorizationService.getCredential();
+			blogger = new Blogger.Builder(new NetHttpTransport(),
+					new JacksonFactory(), credential).setApplicationName(
+					"Blogger").build();
+		}
 
-    public Blogger getBlogger() throws IOException, InvalidValueException {
-        if(null == blogger){
-            GoogleCredential credential = authorizationService.getCredential();
-            blogger =  new Blogger.Builder(
-                    new NetHttpTransport(),
-                    new JacksonFactory(), credential)
-                    .setApplicationName("Blogger")
-                    .build();
-        }
+		return blogger;
+	}
 
-        return blogger;
-    }
+	/**
+	 * Get the blogger name
+	 *
+	 * @return the blogger name
+	 */
+	public String getBloggerName() throws Exception {
+		try {
+			String result = "";
+			logger.config("Get the google credential");
 
+			logger.info("Process The blogger info");
+			Blogger blogger = getBlogger();
+			for (String blogId : BloggerUtil.BLOGGERIDS) {
+				// The BlogId for the http://code.blogger.com/ blog.
+				// This is the request action that you can configure before
+				// sending the request.
+				Blogger.Blogs.Get blogGetAction = blogger.blogs().get(blogId);
+				// Restrict the result content to just the data we need.
+				blogGetAction
+						.setFields("description,name,posts/totalItems,updated");
+				// This step sends the request to the server.
+				Blog blog = blogGetAction.execute();
+				// Now we can navigate the response.
+				System.out.println("Name: " + blog.getName());
+				result = result + "Name: " + blog.getName() + "\t Post Count:"
+						+ blog.getPosts().getTotalItems();
+				System.out.println("Description: " + blog.getDescription());
+				System.out.println("Post Count: "
+						+ blog.getPosts().getTotalItems());
+				System.out.println("Last Updated: " + blog.getUpdated());
+			}
+			return result;
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw e;
+		}
+	}
 
-    /**
-     * Get the blogger name
-     *
-     * @return the blogger name
-     */
-    public String getBloggerName() throws Exception {
-        try {
-            String result = "";
-            logger.config("Get the google credential");
+	/**
+	 * Synch the Post Data from the blogger
+	 *
+	 * @return a row number
+	 * @throws IOException
+	 * @throws ParseException
+	 */
+	public Integer synchPublicPostDataFromBlogger() throws Exception {
+		try {
+			logger.info("Get the google credential");
+			Blogger blogger = getBlogger();
+			List<Post> results = new ArrayList<Post>();
+			for (String blogId : BloggerUtil.BLOGGERIDS) {
+				results = new ArrayList<Post>();
+				logger.info("Set the related info to get the Posts from Blogger");
+				// The request action.
+				Blogger.Blogs.Get blogGetAction = blogger.blogs().get(blogId);
+				Blog blog = blogGetAction.execute();
+				Blogger.Posts.List postsListAction = blogger.posts().list(
+						blogId);
+				// Restrict the result content to just the data we need.
+				// postsListAction.setFields("items(id,author/displayName,content,published,title,url),nextPageToken");
+				// the row number is 500 in a request
+				postsListAction.setMaxResults(Long.valueOf(500));
+				int i = 0;
+				logger.info("Set the Posts into the result list");
+				// get the time loop
+				int time = (blog.getPosts().getTotalItems() / 500.0) <= 0 ? 1
+						: (int) (Math
+								.ceil(blog.getPosts().getTotalItems() / 500.0));
+				while (i < time) {
+					PostList posts = postsListAction.execute();
+					List<Post> tmpPost = posts
+							.getItems()
+							.stream()
+							.filter(p -> (p.getId() != null && !p.getId()
+									.isEmpty()))
+							.filter(p -> (!p.getContent().contains(
+									"Xem tiếp &gt;&gt;&gt;") && !p.getContent()
+									.contains("Còn tiếp &gt;&gt;&gt;")))
+							.collect(Collectors.toList());
+					results.addAll(tmpPost);
+					System.out.println("-- Next page of posts");
+					System.out.println();
+					if (null == posts.getNextPageToken()) {
+						break;
+					}
+					postsListAction.setPageToken(posts.getNextPageToken());
+					i++;
+				}
+				logger.info("There are " + results.size() + "");
+				for (Post post : results) {
+					PostEntity postEntity = new PostEntity(post.getId(),
+							post.getTitle(), post.getAuthor().getDisplayName(),
+							post.getContent(), post.getPublished(),
+							post.getUrl(), PostStatusEnum.PUBLIC.toString());
+					postDAO.save(postEntity);
+				}
 
-            logger.info("Process The blogger info");
-            Blogger blogger = getBlogger();
-            for (String blogId : BloggerUtil.BLOGGERIDS) {
-                // The BlogId for the http://code.blogger.com/ blog.
-                // This is the request action that you can configure before sending the request.
-                Blogger.Blogs.Get blogGetAction = blogger.blogs().get(blogId);
-                // Restrict the result content to just the data we need.
-                blogGetAction.setFields("description,name,posts/totalItems,updated");
-                // This step sends the request to the server.
-                Blog blog = blogGetAction.execute();
-                // Now we can navigate the response.
-                System.out.println("Name: " + blog.getName());
-                result = result + "Name: " + blog.getName() + "\t Post Count:" + blog.getPosts().getTotalItems();
-                System.out.println("Description: " + blog.getDescription());
-                System.out.println("Post Count: " + blog.getPosts().getTotalItems());
-                System.out.println("Last Updated: " + blog.getUpdated());
-            }
-            return result;
-        }  catch (Exception e) {
-            e.printStackTrace();
-            throw e;
-        }
-    }
+			}
+			return results.size();
+		} catch (IOException e) {
+			e.printStackTrace();
+			throw e;
+		}
+	}
 
-    /**
-     * Synch the Post Data from the blogger
-     *
-     * @return a row number
-     * @throws IOException
-     * @throws ParseException
-     */
-    public Integer synchPublicPostDataFromBlogger() throws Exception {
-        try {
-            logger.config("Get the google credential");
-            Blogger blogger =  getBlogger();
-            List<Post>  results = new ArrayList<Post>();
-            for (String blogId : BloggerUtil.BLOGGERIDS) {
-                results = new ArrayList<Post>();
-                logger.config("Set the related info to get the Posts from Blogger");
-                // The request action.
-                Blogger.Blogs.Get blogGetAction = blogger.blogs().get(blogId);
-                Blog blog = blogGetAction.execute();
-                Blogger.Posts.List postsListAction = blogger.posts().list(blogId);
-                // Restrict the result content to just the data we need.
-//                postsListAction.setFields("items(id,author/displayName,content,published,title,url),nextPageToken");
-                // the row number is 500 in a request
-                postsListAction.setMaxResults(Long.valueOf(500));
-                int i = 0;
-                logger.config("Set the Posts into the result list");
-                //get the time loop
-                int time = (blog.getPosts().getTotalItems() / 500.0)  <= 0 ? 1 : (int) (Math.ceil(blog.getPosts().getTotalItems() / 500.0));
-                while (i < time) {
-                    PostList posts = postsListAction.execute();
-                    List<Post> tmpPost = posts.getItems().stream()
-                            .filter(p -> (p.getId() != null && !p.getId().isEmpty()))
-                            .filter(p -> (!p.getContent().contains("Xem tiếp &gt;&gt;&gt;") && !p.getContent().contains("Còn tiếp &gt;&gt;&gt;")))
-                            .collect(Collectors.toList());
-                    results.addAll(tmpPost);
-                    System.out.println("-- Next page of posts");
-                    System.out.println();
-                    if(null == posts.getNextPageToken()){
-                        break;
-                    }
-                    postsListAction.setPageToken(posts.getNextPageToken());
-                    i++;
-                }
-                logger.config("There are " + results.size() + "");
-                for (Post post : results) {
-                    PostEntity postEntity = new PostEntity(
-                            post.getId()
-                            , post.getTitle()
-                            , post.getAuthor().getDisplayName()
-                            , post.getContent()
-                            , post.getPublished()
-                            , post.getUrl()
-                            , PostStatusEnum.PUBLIC.toString()
-                    );
-                    postDAO.save(postEntity);
-                }
+	@Override
+	public List<PostEntity> getAllPostOfBlogger() {
+		List<PostEntity> postEntities = new ArrayList<>();
+		// Page<PostEntity> entities = postDAO.findAll(new PageRequest(1,2));
 
-            }
-            return results.size();
-        } catch (IOException  e) {
-            e.printStackTrace();
-            throw e;
-        }
-    }
-
-    @Override
-    public List<PostEntity> getAllPostOfBlogger() {
-     List<PostEntity> postEntities = new ArrayList<>();
-//      Page<PostEntity> entities = postDAO.findAll(new PageRequest(1,2));
-     
-//        for(PostEntity entity : entities){
-//            postEntities.add(entity);
-//        }
-        return postDAO.findAll();
-//    	return postEntities;
-    }
+		// for(PostEntity entity : entities){
+		// postEntities.add(entity);
+		// }
+		return postDAO.findAll();
+		// return postEntities;
+	}
 }
